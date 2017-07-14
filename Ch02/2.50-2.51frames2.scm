592ecb160636bb031bc32332fd460b36cc3c09b5
Ashi <ashrafeshghi@Tania-Rezais-MacBook.local>
2017-07-13 14:19:48 -1000

    updated frames2

--- Ch02/2.50-2.51frames2.scm
+++ Ch02/2.50-2.51frames2.scm
@@ -112,6 +112,34 @@
 
 (define (below painter1 painter2)
   (let ((split-point (make-vect 0.0 0.5)))
-    (let ((paint-top (transform-painter painter
+    (let ((paint-top (transform-painter painter1
                                         split-point
-                                        (make-vect
+                                        (make-vect 1.0 0.5)
+					(make-vect 0.0 1.0)))
+	  (paint-bottom (transform-painter painter2
+					   (make-vect 0.0 0.0)
+					   (make-vect 1.0 0.0)
+					   split-point)))
+      (lambda (frame)
+	(paint-top frame)
+	(paint-bottom frame)))))
+
+(define (beside painter1 painter2)
+  (let ((split-point (make-vect 0.5 0.0)))
+  (let ((paint-left (transform-painter
+                    painter1
+                    (make-vect 0.0 0.0)
+                    split-point
+                    (make-vect 0.0 1.0)))
+        (paint-right (transform-painter
+                     painter2
+                     split-point
+                     (make-vect 1.0 0.0)
+                     (make-vect 0.5 1.0))))(lambda (frame) (paint-left frame) (paint-right frame)))))
+
+(define (alternate-below painter1 painter2)
+  (let ((top (rotate270 painter1))
+	(bottom (rotate270 painter2)))
+    (rotate90 (beside bottom top))))
+
+(paint (alternate-below einstein wave))
