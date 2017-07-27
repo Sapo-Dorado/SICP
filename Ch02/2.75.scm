f76927679ea8ab884f43785ed30504c4350604e8
Nicholas Brown <countdooku.steak@gmail.com>
2017-07-27 12:45:50 -0700

    2.75.scm

---
+++ Ch02/2.75.scm
@@ -0,0 +1,9 @@
+(define (make-real-from-mag-ang x y)
+  (define (dispatch op)
+    (cond ((eq? op 'real-part) (* x (cos y)))
+          ((eq? op 'imag-part) (* x (sin y)))
+          ((eq? op 'magnitude) x)
+          ((eq? op 'angle) y)
+          (else (error "unknown operation" op))))
+  dispatch)
+
