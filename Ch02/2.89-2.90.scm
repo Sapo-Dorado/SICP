a2dc065e30c62af2029c32248485cc9d2967a777
Nicholas Brown <countdooku.steak@gmail.com>
2017-08-01 09:40:39 -0700

    Fixed bug with dense polynomials

--- Ch02/2.89-2.90.scm
+++ Ch02/2.89-2.90.scm
@@ -25,12 +25,17 @@
     (define (order terms) (car (car terms)))
     (define (coeff term) (cadr term))
     (define (make-term order coeff) (list order coeff))
+    (define (adjoin-term term term-list)
+      (if (=zero? (coeff term))
+          term-list
+          (cons term term-list)))
     (define (tag x) (attach-tag 'sparse x))
     (put 'variable '(sparse) variable)
     (put 'term-list '(sparse) term-list)
     (put 'order 'sparse order)
     (put 'coeff 'sparse coeff)
     (put 'make-term 'sparse make-term)
+    (put 'adjoin-term 'sparse adjoin-term)
     (put 'make 'sparse
         (lambda (var terms) (tag (make-sparse var terms))))
     (put 'make-term 'sparse make-term))
@@ -43,12 +48,15 @@
     (define (order terms) (- (length terms) 1))
     (define (coeff term) term)
     (define (make-term order coeff) coeff)
+    (define (adjoin-term term term-list)
+      (cons term term-list))
     (define (tag x) (attach-tag 'dense x))
     (put 'variable '(dense) variable)
     (put 'term-list '(dense) term-list)
     (put 'order 'dense order)
     (put 'coeff 'dense coeff)
     (put 'make-term 'dense make-term)
+    (put 'adjoin-term 'dense adjoin-term)
     (put 'make 'dense 
          (lambda (var terms) (tag (make-dense var terms))))
     (put 'make-term 'dense make-term))
@@ -84,7 +92,7 @@
             (else
             (let ((t1 (first-term L1))
                   (t2 (first-term L2))
-                  (picked-type (pick-type (type L1) (type L2))))
+                  (picked-type (pick-type type1 type2)))
               (cond ((> (order L1 type1) (order L2 type2))
                       (adjoin-term
                       (make-term (order L1 type1) (coeff t1 type1) picked-type) (add-terms (rest-terms L1) L2 type1 type2) picked-type))
@@ -100,9 +108,9 @@
                                 (rest-terms L2) type1 type2) picked-type)))))))
 
   (define (mul-terms L1 L2 type1 type2)
-    (if (empty-termlist? L1)
+   (if (empty-termlist? L1)
       (the-empty-termlist)
-      (let ((picked-type (pick-type type1 type2)))
+      (let ((picked-type 'sparse))
         (add-terms (mul-term-by-all-terms L1 L2 type1 type2)
                   (mul-terms (rest-terms L1) L2 type1 type2) picked-type picked-type))))
 
@@ -111,16 +119,14 @@
           (the-empty-termlist)
           (let ((t2 (first-term L))
                 (t1 (first-term orderList))
-                (picked-type (pick-type type1 type2)))
+                (picked-type 'sparse))
             (adjoin-term
             (make-term (+ (order orderList type1) (order L type2))
                         (mul (coeff t1 type1) (coeff t2 type2)) picked-type)
            (mul-term-by-all-terms orderList (rest-terms L) type1 type2) picked-type))))
       
   (define (adjoin-term term term-list type)
-    (if (=zero? (coeff term type))
-        term-list
-        (cons term term-list)))
+    ((get 'adjoin-term type) term term-list))
 
   (define (negate-terms list-terms type1)
     (if (null? list-terms)
@@ -136,7 +142,7 @@
       (error "Poly's not in same var: ADD-POLY" (list p1 p2))))
   (define (mul-poly p1 p2)
     (if (same-variable? (variable p1) (variable p2))
-      ((make-type (pick-type (type p1) (type p2))) (variable p1) 
+      ((make-type 'sparse) (variable p1) 
                  (mul-terms (term-list p1) (term-list p2) (type p1) (type p2)))
       (error "Poly's not in same var: ADD-POLY" (list p1 p2))))
 
@@ -179,4 +185,3 @@
 
 (define (make-term order coeff)
   ((get 'make 'term) order coeff))
-
