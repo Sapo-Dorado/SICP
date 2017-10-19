cc89773a3a33093e9a18ea60f2930700629e976e
Nicholas Brown <countdooku.steak@gmail.com>
2017-10-18 22:09:54 -0700

    added 3.25.scm

--- Ch03/3.25.scm
+++ Ch03/3.25.scm
@@ -1,4 +1,13 @@
 (define (make-table)
+
+  (define placeholder '())
+
+  (define (assoc key records)
+    (let ((records (cdr records)))
+      (cond ((null? records) false)
+            ((equal? key (caar records)) (car records))
+            (else (assoc key records)))))
+
   (let ((local-table (list 'table*)))
     (define (lookup keys table)
       (if (null? table)
@@ -17,23 +26,33 @@
       (define (create-branch keys value table)
         (define (iter key-list)
           (if (null? (cdr key-list))
-            (cons (car key-list) (cons value '()))
-            (cons (car key-list) (cons '() (iter (cdr key-list))))))
-        (set-cdr! table (cons (iter keys) (cdr table))))
+            (list (car key-list) value)
+            (cons (car key-list) (list placeholder (iter (cdr key-list))))))
+        (if (null? keys)
+          (set-cdr! table (list value))
+          (let ((new-branch (iter keys)))
+            (if (null? (cdr table))
+              (set-cdr! table (cons placeholder (list new-branch)))
+              (set-cdr! (cdr table) (cons new-branch (cddr table)))))))
 
-      (if (null? table)
-        (create-branch keys value local-table)
+      (define (iter keys table)
         (if (null? keys)
           (set-car! table value)
           (let ((sub-table (assoc (car keys) table)))
-            (if subtable
-              (insert! (cdr keys) value (cdr sub-table))
+            (if sub-table
+              (if (null? (cddr sub-table))
+                (create-branch (cdr keys) value sub-table)
+                (iter (cdr keys) (cdr sub-table)))
               (create-branch keys value table)))))
+
+
+      (if (or (null? table) (null? (cdr table)))
+        (create-branch keys value local-table)
+        (iter keys table))
       (print-table))
 
     (define (print-table)
-      (newline)
-      (display local-table))
+      local-table)
 
     (define (dispatch m)
       (cond ((eq? m 'lookup) (lambda (keys) (lookup keys (cdr local-table))))
@@ -42,16 +61,17 @@
             (else (error "Unknown operation: TABLE" m))))
     dispatch))
 
-(define (create-branch keys value table)
-  (define (iter key-list)
-    (if (null? (cdr key-list))
-      (cons (car key-list) (cons value '()))
-      (cons (car key-list) (cons '() (iter (cdr key-list))))))
-  (set-cdr! table (cons (iter keys) (cdr table))))
+(define table (make-table))
+
+((table 'insert!) '(a b) 'd)
+((table 'insert!) '(a b c) 'r)
+((table 'insert!) '(b a) 'j)
+((table 'insert!) '(b a g) 'h)
+((table 'insert!) '() 'k)
+((table 'lookup) '(a b))
+((table 'lookup) '(a b c))
+((table 'lookup) '(b a))
+((table 'lookup) '(b a g))
+((table 'lookup) '())
 
 
-(define table (list 'table))
-
-(create-branch (list 'a 'b) 'c table)
-
-table
