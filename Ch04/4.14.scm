109715a99c3274386d5363d063c976055d5d9cdd
Nicholas Brown <countdooku.steak@gmail.com>
2018-02-19 19:28:33 -0800

    renamed evaluator and created 4.14-4.15

---
+++ Ch04/4.14.scm
@@ -0,0 +1,3 @@
+;;the scheme primitive map will not work because the arguments to map when evaluated results in a list starting with 'primitive which is not the proper format
+;;for scheme's built in map function
+
---
+++ Ch04/4.15.scm
@@ -0,0 +1,1 @@
+;;It will not work because if (try try) halts (halts? try try) will be true resulting in (try try) running forever creating a paradoxical situation
